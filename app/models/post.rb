8bc97e3a74cf0df884eab2b010c4252cec857570
Piper Gragg <pgragg@dwight.edu>
2015-03-28 16:07:14 -0400

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   default_scope { order('created_at DESC') }
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
+  scope :ordered_by_title,              -> { order('title ASC') }
 end
+
+
+
+# An ordered_by_title scope.
+
+# An ordered_by_reverse_created_at scope.
