109aa345791452eef8af6973710a5e70c7cb5e57
Piper Gragg <pgragg@dwight.edu>
2015-04-05 13:25:03 -0400

    Refactored post editing and creation with partials.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
-  def my_name
-    "Piper Gragg"
-  end
+   def form_group_tag(errors, &block)
+     if errors.any?
+       content_tag :div, capture(&block), class: 'form-group has-error'
+     else
+       content_tag :div, capture(&block), class: 'form-group'
+     end
+   end
 end
+
+#&block is a proc object. 
+# Helpers are in Ruby and often return HTML. 
+# The content_tag is one such method, which takes a symbol argument, a block, and an options hash. 
+# It then creates the symbol-specified HTML tag with the block contents 
+# (and, if specified, the options).
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,11 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
 
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
+
 
   default_scope { order('created_at DESC') }
   scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
@@ -12,6 +17,4 @@ end
 
 
 
-# An ordered_by_title scope.
-
-# An ordered_by_reverse_created_at scope.
+#There are numerous types of validations. The most common are presence, length, and format. Formatting usually requires a Regex, which is an advanced topic that will be covered later. Here's the Rails Guide on validations - it's a good reference.
