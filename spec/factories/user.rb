0af63c536fb160b27195cea42df1a151c8cd8f05
Piper Gragg <pgragg@dwight.edu>
2015-05-21 16:53:45 -0400

    Testing Users Assignment. Iffy.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,8 +5,20 @@ FactoryGirl.define do
      password "helloworld"
      password_confirmation "helloworld"
      confirmed_at Time.now
-   end
- end
+
+     factory :user_with_post_and_comment do
+     user = create(:user)
+     first_name "Admin"
+     last_name  "User"
+     admin      true
+
+      after(:build) do |user|
+         post = user.posts.create(:post)
+         user.posts.first.comments.create(:comment)
+      end
+    end
+  end
+end
 
 # We can use the above factory like so: 
 # build(:user) # Initialize but do not save
